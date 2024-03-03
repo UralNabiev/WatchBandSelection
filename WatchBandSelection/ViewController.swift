3a41e99755612364948bcfa0ed7ef9b6722a86be
UralNabiev <devuralnabiev@gmail.com>
2024-03-03 17:51:54 +0500

    Initial Commit

---
+++ WatchBandSelection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchBandSelection
+//
+//  Created by O'ral Nabiyev on 03/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
